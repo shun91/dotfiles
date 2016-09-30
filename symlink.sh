66e7388503b3ef8f08ecf07d577560e0af1636bf
shun91 <shun9167@gmail.com>
2016-09-30 18:03:19 +0900

    Update symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
