e52772b9626a18bde770377e90ea3b05d5c1fa77
shun91 <shun9167@gmail.com>
2019-05-26 17:38:57 +0900

    ♻️ fix comment

--- setup.sh
+++ setup.sh
@@ -1,29 +1,41 @@
 #!/bin/sh
 
-### 各ファイルにsymlinkをはる ###
+############################################################################
+### 複数回実行されたら困る処理を書かないこと！！！                       ###
+### FIXME: 複数回実行を許す処理を初めの1回だけの処理でファイルを分けたい ###
+############################################################################
+
+# 各 dotfile のシンボリックリンク作成
+# FIXME: リポジトリ内の dotfile を自動で取得できるようにしたい
+# ------------------------------------------------------------------------------
 ln -snf ~/dotfiles/.zshrc ~/.zshrc
 ln -snf ~/dotfiles/.vimrc ~/.vimrc
 ln -snf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -snf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -snf ~/dotfiles/.gitconfig ~/.gitconfig
 
-# 可能な場合はremoteリポジトリはssh化しておく（push時にpass等の入力を省略できる）
+# remote リポジトリの ssh 化
+# ------------------------------------------------------------------------------
 cd ~/dotfiles
 git remote set-url origin git@github.com:shun91/dotfiles.git
 
-# ~/.zshrc_localが存在しなければ作成
+# ~/.zshrc_local が存在しなければ作成
+# ------------------------------------------------------------------------------
 if [ ! -e ~/.zshrc_local ]; then
   touch ~/.zshrc_local
 fi
 
-### ゴミ箱ディレクトリ作成 ###
+# ゴミ箱ディレクトリ作成 (rm するとこのディレクトリへ mv される)
+# ------------------------------------------------------------------------------
 mkdir -p ~/.Trash
 
-### vscodeの設定ファイルをシンボリックリンク
+# vscode の設定ファイルのシンボリックリンク作成
+# ------------------------------------------------------------------------------
 \rm -rf ~/Library/Application\ Support/Code/User
 ln -s ~/dotfiles/vscode/User ~/Library/Application\ Support/Code/User
 
-### zsh plugins
+# zsh plugin のインストール
+# ------------------------------------------------------------------------------
 # zsh-autosuggestions
 git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 # zsh-syntax-highlighting
