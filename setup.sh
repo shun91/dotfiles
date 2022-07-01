1543ee9336ff8f2b0b524ee6954896fc5d63e644
Shun Kawahara <shun9167@gmail.com>
2022-07-01 18:15:31 +0900

    fix git url

--- setup.sh
+++ setup.sh
@@ -37,6 +37,6 @@ ln -s ~/dotfiles/vscode/User ~/Library/Application\ Support/Code/User
 # zsh plugin のインストール
 # ------------------------------------------------------------------------------
 # zsh-autosuggestions
-git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 # zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
